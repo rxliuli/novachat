862eb97d7d437ee65afc1e97e7bd5bcd28c74a11
rxliuli <rxliuli@gmail.com>
2025-01-06 20:10:27 +0800

    chore: up @rxliuli/vista

--- src/lib/plugins/client/addInterceptor.ts
+++ src/lib/plugins/client/addInterceptor.ts
@@ -1,23 +1,19 @@
-import {
-  type InterceptOptions,
-  interceptFetch,
-  interceptXHR,
-} from '../interceptors'
 import { minimatch } from 'minimatch'
+import { Vista, type Middleware } from '@rxliuli/vista'
 
 // const EXCLUDE_HOSTNAME = ['127.0.0.1', 'localhost']
 const INCLUDE_HOSTNAME = ['*.googleapis.com', 'api.anthropic.com']
 
-const newLocal: InterceptOptions = {
-  request(req) {
-    const hostname = new URL(req.url).hostname
-    if (INCLUDE_HOSTNAME.some((it) => minimatch(hostname, it))) {
-      const redirectUrl =
-        import.meta.env.VITE_REVERSE_PROXY_URL + '?url=' + req.url
-      return new Request(redirectUrl)
-    }
-    return req
-  },
+const proxy: () => Middleware = () => async (c, next) => {
+  const hostname = new URL(c.req.url).hostname
+  if (INCLUDE_HOSTNAME.some((it) => minimatch(hostname, it))) {
+    const redirectUrl =
+      import.meta.env.VITE_REVERSE_PROXY_URL + '?url=' + c.req.url
+    c.req = new Request(redirectUrl, c.req)
+  }
+  await next()
 }
-interceptFetch(newLocal)
-interceptXHR(newLocal)
+
+const vista = new Vista()
+vista.use(proxy())
+vista.intercept()
